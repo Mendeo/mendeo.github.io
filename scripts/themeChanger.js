30bd061341e44abe843f24883b553154d579c7f9
Themen <thesolve@mail.ru>
2020-11-14 23:01:56 +0300

    Сделал кнопку для переключения темы.

--- scripts/themeChanger.js
+++ scripts/themeChanger.js
@@ -6,15 +6,66 @@
 {
 	'use strict';
 	const styleTag = document.getElementById('mainStyle');
+	const button = document.getElementById('themeChangeButton');
+
+	let isDark = false;
+	const THEME_STORAGE_NAME = 'themeSelected';
+	const STORAGE_LIGHT_THEME = 'light';
+	const STORAGE_DARK_THEME = 'dark';
+	let selectedTheme = localStorage.getItem(THEME_STORAGE_NAME);
+	if (selectedTheme)
+	{
+		if (selectedTheme === STORAGE_LIGHT_THEME)
+		{
+			isDark = false;
+		}
+		else if (selectedTheme === STORAGE_DARK_THEME)
+		{
+			isDark = true;
+		}
+		else
+		{
+			setThemeToLocalStorage(false);
+		}
+	}
+	else
+	{
+		setThemeToLocalStorage(false);
+	}
+
+	function setThemeToLocalStorage(isDark)
+	{
+		if (isDark)
+		{
+			localStorage.setItem(THEME_STORAGE_NAME, STORAGE_DARK_THEME);
+			isDark = true;
+		}
+		else
+		{
+			localStorage.setItem(THEME_STORAGE_NAME, STORAGE_LIGHT_THEME);
+			isDark = false;
+		}
+	}
+
+	changeTheme(isDark);
+	button.addEventListener('click', () => 
+	{
+		isDark = !isDark;
+		changeTheme(isDark);
+	});
+	
 	function changeTheme(isDark)
 	{
 		if (isDark)
 		{
 			styleTag.href = '{% link assets/css/style_dark.scss %}';
+			button.innerHTML = 'Светлая тема';
 		}
 		else
 		{
 			styleTag.href = '{% link assets/css/style_light.scss %}';
+			button.innerHTML = 'Тёмная тема';
 		}
+		setThemeToLocalStorage(isDark);
 	}
 })();
