66786856571607f0df806ff56c2a64e9a9241f4a
Themen <thesolve@mail.ru>
2021-05-18 16:43:48 +0300

    Сделал первую форму для ввода Отладил на способность отлавливать ошибки входных данных

--- scripts/projects/survival.js
+++ scripts/projects/survival.js
@@ -1,5 +1,53 @@
 (function()
 {
 	'use strict';
-	const myButton = document.getElementById('hello_there').addEventListener('click', () => alert('Ура! Ура!'));
+	const remainigAgeElement = document.getElementById('remainingAge');
+	const remainigAgeNameElement = document.querySelector('#remainingAge + span');
+	let rAge = remainingAge(0);
+	remainigAgeElement.innerText = rAge;
+	remainigAgeNameElement.innerText = name(rAge);
+
+	document.getElementById('survival-curent-age-input').addEventListener('input', function()
+	{
+		const age = Number(this.value);
+		if (this.value === '' || Number.isNaN(age) || !Number.isInteger(age) || age < 0 || age > 100)
+		{
+			this.classList.add('input-error');
+			remainigAgeElement.innerText = '__';
+			remainigAgeNameElement.innerText = '';
+		}
+		else
+		{
+			this.classList.remove('input-error');
+			rAge = remainingAge(age);
+			remainigAgeElement.innerText = rAge;
+			remainigAgeNameElement.innerText = name(rAge);
+		}
+	});
+	function remainingAge(curentAge)
+	{
+		return curentAge + 10;
+	}
+	function name(n)
+	{
+		const words = [' лет', ' год', ' года']
+		n %= 100;
+		if (n >= 10 && n <= 20)
+		{
+			return words[0];
+		}
+		else
+		{
+			n %= 10;
+			switch(n)
+			{
+				case 0: case 5: case 6: case 7: case 8: case 9:
+					return words[0];
+				case 1:
+					return words[1];
+				case 2: case 3: case 4:
+					return words[2];
+			}
+		}
+	}
 })();
