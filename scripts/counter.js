a2ff4db015ed06d66ad42f19cb4e2775f58a70be
Themen <thesolve@mail.ru>
2025-10-09 16:35:24 +0300

    Убрал всю аналитику и соответствующую плашку про cookie.

--- scripts/counter.js
+++ scripts/counter.js
@@ -2,13 +2,13 @@
 layout: js_minifier
 ---
 
-(function()
-{
-	'use strict';
-	const COUNTED_STORAGE_NAME = 'counted';
-	if (!localStorage.getItem(COUNTED_STORAGE_NAME))
-	{
-		localStorage.setItem(COUNTED_STORAGE_NAME, 'true');
-		navigator.sendBeacon(MESSAGE_SERVER, JSON.stringify({counter: true}));
-	}
-})();
+// (function()
+// {
+// 	'use strict';
+// 	const COUNTED_STORAGE_NAME = 'counted';
+// 	if (!localStorage.getItem(COUNTED_STORAGE_NAME))
+// 	{
+// 		localStorage.setItem(COUNTED_STORAGE_NAME, 'true');
+// 		navigator.sendBeacon(MESSAGE_SERVER, JSON.stringify({counter: true}));
+// 	}
+// })();
